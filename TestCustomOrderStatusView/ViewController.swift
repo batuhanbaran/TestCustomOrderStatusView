bb0ac422fa591c2cc273d92aa5b84e16b91f31c4
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-06-18 12:09:03 +0300

    Initial Commit

---
+++ TestCustomOrderStatusView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCustomOrderStatusView
+//
+//  Created by Batuhan Baran on 18.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
